abf45edf5388933ec2f187c61cf190e4a057fd59
Jordan Willis <jsw0002@gmail.com>
2016-05-23 16:54:40 -0400

    added methods if else statements, code blocks, operators, and keywords

---
+++ KeywordsAndExpressions/src/com/jordanwillis/Main.java
@@ -0,0 +1,21 @@
+package com.jordanwillis;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highscore = 50;
+
+        if (highscore == 50)
+            System.out.println("This is an expression");
+
+        // In the following code that I will type below, write down what parts of the code
+        // are expressions.
+        int score = 100; // score = 100
+        if (score > 99) { // score > 99
+            System.out.println("You got the high score!"); // "You got the high score!"
+            score = 0; // score = 0
+        }
+    }
+}
