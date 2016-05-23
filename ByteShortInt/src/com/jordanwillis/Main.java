c7af6992d451476febb23990833c446fdb8025bc
Jordan Willis <jsw0002@gmail.com>
2016-05-23 14:25:02 -0400

    learned about int, floats, decimals, bytes, and shorts

--- ByteShortInt/src/com/jordanwillis/Main.java
+++ ByteShortInt/src/com/jordanwillis/Main.java
@@ -3,6 +3,35 @@ package com.jordanwillis;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // int has a width of 32
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+        int myTotal = (myMinValue/2);
+        System.out.println("myTotal = " + myTotal);
+
+        // byte has a width of 8
+        byte myByteValue = -128; // -128 to 127
+        byte myNewByteValue = (byte)(myByteValue/2);
+        System.out.println("myNewByteValue = " + myNewByteValue);
+
+        // short has a width of 16
+        short myShortValue = 32767; // -32768 to 32767
+        short myNewShortValue = (short)(myShortValue/2);
+
+        // long has a width of 64
+        long myLongValue = 9_223_372_036_854_775_807L; // -9223372036854775808 to 9223372036854775807
+
+        // 1. Create a byte variable and set it to any valid byte number.
+        // 2. Create a short variable and set it to any valid short number.
+        // 3. Create a int variable and set it to any valid int number.
+        // 4. Create a variable of type long, and make it equal to
+        //    50000 + 10 times the sum of byte plus the short plus the int.
+
+        byte myByte = 99;
+        short myshort = 27520;
+        int myInt = 565632;
+        long myLong = 50000L + 10L * (myByte + myshort + myInt);
+        System.out.println("myLong = " + myLong);
     }
 }
---
+++ FloatAndDouble/src/com/jordanwillis/Main.java
@@ -0,0 +1,28 @@
+package com.jordanwillis;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // width of int = 32 (4 bytes).
+        int myIntValue = 5 / 3;
+        // width of float = 32 (4 bytes).
+        float myFloatValue = 5f / 3f;
+        // width of double = 64 (8 bytes).
+        double myDoubleValue = 5d / 3d;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+        // Convert a given number of pounds to kilograms
+        // 1. Create a variable to store the number of pounds
+        // 2. Calculate the number of kilograms for the number above and store in a variable
+        // 3. Print out the result.
+        //
+        // NOTES: 1 pound is equal to 0.45359237 kilograms.
+
+        double weightInPounds = 200d;
+        double weightInKilograms = weightInPounds * 0.45359237d;
+        System.out.println("weightInKilograms = " + weightInKilograms);
+    }
+}
