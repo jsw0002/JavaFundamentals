04050f5cb6e8c57dbcd083f3db7f94efb5048764
Jordan Willis <jsw0002@gmail.com>
2016-06-01 11:39:13 -0400

    added inner class example

--- Inner/src/com/jordanwillis/Main.java
+++ Inner/src/com/jordanwillis/Main.java
@@ -1,8 +1,66 @@
 package com.jordanwillis;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+
     public static void main(String[] args) {
-	// write your code here
+//        Gearbox mcLaren = new Gearbox(6);
+//
+//        mcLaren.operateClutch(true);
+//        mcLaren.changeGear(1);
+//        mcLaren.operateClutch(false);
+//        System.out.println(mcLaren.wheelSpeed(1000));
+//        mcLaren.changeGear(2);
+//        System.out.println(mcLaren.wheelSpeed(3000));
+//        mcLaren.operateClutch(true);
+//        mcLaren.changeGear(3);
+//        mcLaren.operateClutch(false);
+//        System.out.println(mcLaren.wheelSpeed(6000));
+//        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+//        System.out.println(first.driveSpeed(1000));
+//
+//      below is a class within the main class
+//
+//            class ClickListener implements Button.OnClickListener{
+//                public ClickListener(){
+//                    System.out.println("I've been attached");
+//                }
+//
+//                @Override
+//                public void onClick(String title){
+//                    System.out.println(title + " was clicked");
+//                }
+//            }
+//
+//        btnPrint.setOnClickListener(new ClickListener());
+
+        // Below is an anonymous class
+
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        });
+        listen();
+    }
+
+    private static void listen(){
+        boolean quit = false;
+        while (!quit){
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
+        }
     }
 }
---
+++ InnerClassesChallenge/src/com/jordanwillis/Song.java
@@ -0,0 +1,23 @@
+package com.jordanwillis;
+
+/**
+ * Created by Jordan on 6/1/2016.
+ */
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
