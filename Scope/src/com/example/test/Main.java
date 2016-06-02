a732cf1cc34d50bb317c91fbc6c81fa1df2ef212
Jordan Willis <jsw0002@gmail.com>
2016-06-02 15:15:12 -0400

    added scope examples

--- Scope/src/com/example/test/Main.java
+++ Scope/src/com/example/test/Main.java
@@ -3,6 +3,22 @@ package com.example.test;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String varFour = "This is private to main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+        scopeInstance.useInner();
+
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+        // Can't do below when the variable is private
+//        System.out.println("varThree is not accessible here " + innerClass.varThree);
+
+//        System.out.println("scopeInstance varOne is " + scopeInstance.getVarOne());
+//        System.out.println(varFour);
+//
+//        scopeInstance.timesTwo();
+//        System.out.println("***************************************");
+//
+//        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+//        innerClass.timesTwo();
     }
 }
